a25ba9852e943ef1e4b9cb2c19137b0b60b58d83
MrTj458 <mrtj458@gmail.com>
2018-08-20 15:39:19 -0600

    Added vowel count

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ function sumArray(array) {
 function fToC(degreesF) {
   return (degreesF - 32) * (5 / 9)
 }
+
+function vowelCount(input) {
+  var totalVowels = 0
+  var vowels = ['a', 'e', 'i', 'o', 'u']
+  var letters = input.toLowerCase().split('')
+
+  for (var letter = 0; letter < letters.length; letter++) {
+    for (var vowel = 0; vowel < vowels.length; vowel++) {
+      if (letters[letter] === vowels[vowel]) {
+        totalVowels++
+      }
+    }
+  }
+
+  return totalVowels
+}
